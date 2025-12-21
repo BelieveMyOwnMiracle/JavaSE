76e07f86071a8f09a0d787f067306c635cab1777
BelieveMyOwnMiracle <1371996959@qq.com>
2025-12-21 20:49:37 +0800

    commit

--- src/collection/LinkedStack.java
+++ src/collection/LinkedStack.java
@@ -1,4 +1,18 @@
 package collection;
 
-public class LinkedStack {
+public class LinkedStack<E> {
+    private final Node<E> head = new Node<>(null);
+
+    private static class Node<E>{
+        E element;
+        Node<E> next;
+
+        public Node(E element) {
+            this.element = element;
+        }
+    }
+
+
+
+
 }
--- src/collection/Test.java
+++ src/collection/Test.java
@@ -2,6 +2,18 @@ package collection;
 
 public class Test {
     public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            arrayList.add(i+"",i);
+        }
+        System.out.println(arrayList);
 
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(10,0);
+        linkedList.add(20,1);
+        linkedList.add(30,0);
+
+        System.out.println(linkedList);
+        System.out.println(linkedList.get(0));
     }
 }
